3d2cd0466d09a6e30f761eab824fa295c7843338
ericboucher <eric.p.boucher@gmail.com>
2026-04-28 13:43:54 +0200

    Fix redirect bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -25,6 +25,7 @@ const App = () => (
           <Route path="/approche" element={<Approche />} />
           <Route path="/situations" element={<Situations />} />
           <Route path="/a-propos" element={<APropos />} />
+          <Route path="/qui-je-suis" element={<Navigate to="/a-propos" replace />} />
           <Route path="/mentions-legales" element={<MentionsLegales />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
